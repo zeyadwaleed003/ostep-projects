4e3fe4fcb04c7d094988a2d18f9ca01a1570da85
Zeyad <zeyadw526@gmail.com>
2025-05-03 12:24:56 +0300

    wunzip finished and passed the tests

---
+++ initial-utilities/wunzip/wunzip.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        printf("wunzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    int cnt;
+    char c;
+    for(int i = 1; i < argc; ++i){
+        FILE *f = fopen(argv[1], "r");
+
+        if(f == NULL){
+            printf("wzip: cannot open file\n");
+	        exit(1);
+        }
+        else{
+            while(fread(&cnt, sizeof(int), 1, f) != 0){
+                fread(&c, sizeof(char), 1, f);
+
+                for(int j = 0; j < cnt; ++j){
+                    printf("%c", c);
+                }
+            }
+        }
+    
+        fclose(f);  
+    }    
+    return 0;
+}
